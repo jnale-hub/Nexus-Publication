fcbfdab611a2a857c938f865a0b095993d10a631
John Mark Delima <delimajohnmark14@gmail.com>
2023-08-12 09:21:26 +0800

    Troubleshoot css

--- nexus/settings.py
+++ nexus/settings.py
@@ -117,8 +117,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, images)
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
+STATICFILES_DIRS = [BASE_DIR / "static"]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
