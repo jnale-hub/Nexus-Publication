c06cc74ca7bc5b92cf4120362d3a62e8d5fd07db
John Mark Delima <delimajohnmark14@gmail.com>
2023-08-12 11:32:28 +0800

    Troubleshoot css and js

--- nexus_games/urls.py
+++ nexus_games/urls.py
@@ -5,3 +5,5 @@ urlpatterns = [
     path('', views.games, name='games'),
     path('wordle/', views.wordle, name='wordle'),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
